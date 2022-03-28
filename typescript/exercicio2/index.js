17534848eed4c4351e21961b92d576c28fc6b071
TiagoMarinhoSousa <tiagomarinho9101@gmail.com>
2022-03-28 15:50:36 -0300

    exercicio 2

---
+++ typescript/exercicio2/index.js
@@ -0,0 +1,12 @@
+var var1 = "texto";
+var var2 = 5;
+var var3 = true;
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+//comentado esse trecho para prosseguir os exercicios sem erros
+// var1 = 3;
+// var2 = true;
+// var3 = "texto";
+//Foi utilizada a inferência de tipos para passar a informação de qual tipo de dados cada variável poderá receber, mesmo o tipo não estando explicito....
+//Na sequência apresenta erro nas linhas 10, 11 e 12, pq tenta atribuir um tipo diferente as variáveis 
