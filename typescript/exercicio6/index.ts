20b20699f2811a367e31685f1cb752a3eda6abeb
TiagoMarinhoSousa <tiagomarinho9101@gmail.com>
2022-04-07 17:26:12 -0300

    Exercicio 6

---
+++ typescript/exercicio6/index.ts
@@ -0,0 +1,45 @@
+class Conta {
+    private agencia: string;
+    private numConta: string;
+    private saldo: number;
+
+    constructor(agencia: string, numConta: string, saldo: number){
+        this.agencia = agencia;
+        this.numConta = numConta;
+        this.saldo = saldo;
+    }
+
+    get getAgencia(): string{
+        return this.agencia;
+    }
+
+    set setAgencia(agencia: string){
+        this.agencia = agencia;
+    }
+
+    
+    get getNumConta(): string{
+        return this.numConta;
+    }
+
+    set setNumConta(numConta: string){
+        this.numConta = numConta;
+    }
+
+    get getSaldo(): number{
+        return this.saldo;
+    }
+}
+
+const conta = new Conta('1234', '987654', 500);
+console.log('Agencia: ' + conta.getAgencia);
+console.log('Conta: ' + conta.getNumConta);
+console.log('Saldo: ' + conta.getSaldo);
+
+
+conta.setAgencia = '5678';
+conta.setNumConta = '012345';
+console.log('Nova Agencia: ' + conta.getAgencia);
+console.log('Nova Conta: ' + conta.getNumConta);
+
+export{};
